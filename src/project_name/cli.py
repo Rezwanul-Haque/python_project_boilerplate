c57d2e6856cede2af72a1f3362bc2c602f839b8b
Rezwanul-Haque <rezwanul.cse@gmail.com>
2019-05-22 10:04:44 +0600

    Standard Library import way added.

---
+++ src/project_name/cli.py
@@ -0,0 +1,14 @@
+"""Docstring for the project
+
+   Author: Author_Name
+   Date: 
+   License: 
+"""
+# Import Standard Library
+
+# Import Third Party Library
+
+# Import Local  Library
+
+# Your code goes here
+
